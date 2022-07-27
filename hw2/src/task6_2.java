faceef0571451fa2357bec1c8ea755eca2697639
VitalQaa <vitalqaa77@gmail.com>
2022-07-27 23:10:49 +0300

    add another way to solve task 6

---
+++ hw2/src/task6_2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class task6_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        StringBuilder str = new StringBuilder(sc.nextLine());
+        StringBuilder newStr = new StringBuilder();
+        int offset = 0;
+        int n = 0;
+        int newLine = 0;
+        int counter = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) {
+                newStr.append(str.charAt(i));
+                counter++;
+                if (counter > 1) {
+                    newStr.insert(i+offset, "1");
+                    offset++;
+                }
+            } else {
+                newStr.append(str.charAt(i));
+                counter = 0;
+            }
+        }
+
+        String[] strings = newStr.toString().split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+        for (int i = 1; i < strings.length; i+=2) {
+            n = Integer.parseInt(strings[i-1]);
+            System.out.print(strings[i].repeat(n));
+            newLine+=n;
+            if (newLine > 40) {
+                System.out.println();
+                newLine = 0;
+            }
+        }
+    }
+
+}
+
